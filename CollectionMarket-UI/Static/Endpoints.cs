600b4efe251cb3b6fb4609b1f9429daa3ad216fb
Jakub Pawlak <pawlak.jakub2@interia.pl>
2021-01-02 15:49:04 +0100

    Add base repository

---
+++ CollectionMarket-UI/Static/Endpoints.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollectionMarket_UI.Static
+{
+    public static class Endpoints
+    {
+        public static string BaseUrl = "http://localhost:44342/";
+        public static string UsersEndpoint = $"{BaseUrl}api/users";
+    }
+}
